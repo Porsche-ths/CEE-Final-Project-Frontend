daf6304a8a88e67ad3aee4e4e110801bb9ec6e3e
Teme Sutichairatana <pj_teme@hotmail.com>
2023-04-29 22:21:45 +0700

    script_mcv complete

--- script_mcv.js
+++ script_mcv.js
@@ -2,11 +2,11 @@
 // Change this IP address to EC2 instance public IP address when you are going to deploy this web application
 const backendIPAddress = "127.0.0.1:3000";
 
-// -----------------------------------------------------------------
+// --------------------------------------------------------------------------------------------------------
 
 let jsonCache = new Array;
 
-// -----------------------------------------------------------------
+// --------------------------------------------------------------------------------------------------------
 
 const login_mcv = () => {
   window.location.href = `http://${backendIPAddress}/courseville/login`;
@@ -16,7 +16,7 @@ const logout_mcv = async () => {
   window.location.href = `http://${backendIPAddress}/courseville/logout`;
 };
 
-// -----------------------------------------------------------------
+// --------------------------------------------------------------------------------------------------------
 
 // get user's info and added it to "user-data" (loaded upon login)
 const getUserInfo = async () => {
@@ -31,7 +31,6 @@ const getUserInfo = async () => {
     .then((response) => response.json())
     .then((data) => {
       console.log(data);
-      console.log(data.user);
       document.getElementById("user-data").innerHTML = `
         ${data.data.student.id} ${data.data.student.firstname_th} ${data.data.student.lastname_th} <br> ${data.data.student.firstname_en} ${data.data.student.lastname_en}
       `;
@@ -39,8 +38,7 @@ const getUserInfo = async () => {
     .catch((error) => console.error(error));
 };
 
-let subjects = new Array;
-let ids = new Array;
+// --------------------------------- Fetching into Cache --------------------------------------
 
 // called when user logged into a new session
 const fetchCoursesFromMCV = async () => {
@@ -57,7 +55,8 @@ const fetchCoursesFromMCV = async () => {
       .then((data) => data.data.student)
       .then((courses) => {
           courses.map(course => {
-            const cv_cid = {
+            const fetched_cv_cid = {
+              cv_cid: course.cv_cid,
               title: course.title,
               course_no: course.course_no,
               year: course.year,
@@ -65,15 +64,17 @@ const fetchCoursesFromMCV = async () => {
               course_icon: course.course_icon,
               assignments: getAssignmentsFromSubject(course.cv_cid)
             }
-            jsonCache.push(cv_cid);
+          jsonCache.push(fetched_cv_cid);
           })
       })
       .catch((error) => console.error(error));
       console.log(jsonCache)
 }
 
+// --------------------------------------------------------------------------------------------------------
+
 const getAssignmentsFromSubject = async (cv_cid) => {
-  const allAssignments = new Array;
+  let allAssignments = new Array;
   const options = {
     method: "GET",
     credentials: "include",
@@ -91,101 +92,76 @@ const getAssignmentsFromSubject = async (cv_cid) => {
   return allAssignments;
 }
 
+// --------------------------------------- Select Courses from Semester ----------------------------------------------
+
+let allIdInSemester = new Array;
+
 // get number of semesters & subjects in each semester + show the button according to provided semesters in "semester-modal" (loaded upon login)
-const getSubjectsFromSemester = async (year, semester) => {
-    subjects = [];
-    ids = [];
+function getSubjectsFromSemester(year, semester) {
+    allIdInSemester = jsonCache.filter(id => id.year == String(year) && id.semester == String(semester));
+    console.log(allIdInSemester)
     document.getElementById("subject-modal-info").innerHTML = "";
-    const options = {
-        method: "GET",
-        credentials: "include",
-    }
-    await fetch(`http://${backendIPAddress}/courseville/get_courses`, options)
-        .then((response) => response.json())
-        .then((data) => data.data.student)
-        .then((courses) => {
-            for (let i = 0; i < courses.length; i++) {
-                if (courses[i].year == year && courses[i].semester == semester) {
-                    console.log(courses[i].title);
-                    subjects.push(courses[i].title);
-                    ids.push(courses[i].cv_cid);
-                    document.getElementById("subject-modal-info").innerHTML += `
-                        <label><input type="checkbox" id="${courses[i].cv_cid}"><span class="subject-check"><i></i></span></label><span class="option subject" id=${courses[i].cv_cid}>${courses[i].title}</span><br>
-                    `
-                }
-            }
-        })
-        .catch((error) => console.error(error));
-}
-
-const loopCreateTable = async () => {
-  console.log("I'm in");
-  document.getElementById("assignments-table").innerHTML = `
-      <tr class="table-head">
-          <th class="check-col">Done</th>
-          <th class="assignment-col">Assignment</th>
-          <th class="subject-col">Subject</th>
-          <th class="date-col">Due Date</th>
-          <th class="time-col">Due Time</th>
-          <th>Note</th>
-          <th class="save-col">Save</th>
-      </tr>`;
-    for (let i = 0; i < subjects.length; i++) {
-        let isChecked = document.getElementById(`${ids[i]}`).checked;
-        if (isChecked) {
-          console.log("round " + i);
-          createAssignmentsTable(subjects[i], ids[i]);
-        }
+    for (let id of allIdInSemester) {
+        document.getElementById("subject-modal-info").innerHTML += `
+              <label><input type="checkbox" id="checkbox_${id.course_no}"><span class="subject-check"><i></i></span></label><span class="option subject" id=${id.course_no}>${id.title}</span><br>
+              `
     }
 }
 
-// show all assignments in semester/year
-const createAssignmentsTable = async (subject, cv_cid) => {
-  const options = {
-    method: "GET",
-    credentials: "include",
-  };
+// --------------------------------------------------------------------------------------------------------
 
-    await fetch(`http://${backendIPAddress}/courseville/get_course_assignments/${cv_cid}`, options)
-      .then((response) => response.json())
-      .then((data) => data.data)
-      .then((assignments) => {
-          assignments.map((items) => {
-              let epoch = items.duetime;
-              let currentDate = new Date(epoch*1000); 
-              let localeDateStr = currentDate.toLocaleDateString("en-us", {
-                weekday: "short",
-                year: "numeric",
-                month: "short",
-                day: "numeric"
-              })
-              let localeTimeStr = currentDate.toLocaleTimeString("en-us", {
-                hour12: true,
-                hour: "2-digit",
-                minute: "2-digit",
-              })
-              
-              document.getElementById("assignments-table").innerHTML += `
+let selectedId = new Array;
+
+function createTable() {
+  for (let id of allIdInSemester) {
+    let isChecked = document.getElementById(`checkbox_${id.course_no}`).checked;
+    if (isChecked) {selectedId.push(id)}
+  }
+  showInTable();
+}
+
+// --------------------------------------------------------------------------------------------------------
+
+function showInTable() {
+  for (let each of selectedId) {
+    console.log(each.title)
+    let promise = each.assignments;
+    console.log(promise)
+    promise.then(
+      function(value) {show(value, each.title);},
+      function(error) {show(error, each.title);}
+    )
+  }
+}
+
+function show(x, title) {
+  for (each of x) {
+      let epoch = each.duetime;
+      let currentDate = new Date(epoch*1000); 
+      let localeDateStr = currentDate.toLocaleDateString("en-us", {
+          weekday: "short",
+          year: "numeric",
+          month: "short",
+          day: "numeric"
+      })
+      let localeTimeStr = currentDate.toLocaleTimeString("en-us", {
+          hour12: true,
+          hour: "2-digit",
+          minute: "2-digit",
+      })
+      document.getElementById("assignments-table").innerHTML += `
                     <tr>
-                        <td class="check-col"><label><input type="checkbox" id="checkbox_${items.itemid}"><span><i></i></span></label></td>
-                        <td class="assignment-col">${items.title}</td>
-                        <td class="subject-col">${subject}</td>
+                        <td class="check-col"><label><input type="checkbox" id="checkbox_${each.itemid}"><span><i></i></span></label></td>
+                        <td class="assignment-col">${each.title}</td>
+                        <td class="subject-col">${title}</td>
                         <td class="date-col">${localeDateStr}</td>
                         <td class="time-col">${localeTimeStr}</td>
-                        <td><textarea id="note_${items.itemid}"></textarea></td>
+                        <td><textarea id="note_${each.itemid}"></textarea></td>
                         <td class="save-col">
-                            <button type="submit" class="confirm-button" id="save-changes" onclick=addToTable(${items.itemid})>Save</button>
+                            <button type="submit" class="confirm-button" id="save-changes" onclick=addToTable(${each.itemid})>Save</button>
                             <button type="submit" class="confirm-button" id="cancel-changes">Cancel</button>
                         </td>
                     </tr>
                     `;
-                    
-          })
-      })
-      .catch((error) => console.error(error));
-  /*console.log(
-    "This function should fetch 'get course assignments' route from backend server and show assignments in the table."
-  );*/
-};
-
-
+  }
+}
